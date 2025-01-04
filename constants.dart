ef28154efc853d80f245c520a9bad89a3109a4b4
farxana jafary <farzanajafari17@gmail.com>
2025-01-05 00:02:14 +0330

    Add files via upload

---
+++ constants.dart
@@ -0,0 +1,3 @@
+//TODO: 1.3.3 Put your API key and CoinAPI's base url in the following constants
+const kAPIKey = 'B56219D2-EE24-4817-A893-C95F3A7103E6';
+const kBaseURL = 'https://rest.coinapi.io/v1/exchangerate/';
